3c9b33ab848c97fcc5e85877a4c1cdc7e1e3eb87
Florian Kaufmann <sensorflo@gmail.com>
2012-08-11 00:33:36 +0200

    added URL of hosting site

--- find-file-ext.el
+++ find-file-ext.el
@@ -3,6 +3,7 @@
 ;; Copyright 2009-2012 Florian Kaufmann <sensorflo@gmail.com>
 ;; 
 ;; Author: Florian Kaufmann <sensorflo@gmail.com>
+;; URL: https://gitorious.org/logfile-mode/find-file-ext
 ;; 
 ;; This file is not part of GNU Emacs.
 ;; 
